939aaf775a94f8b3cf2c6746f37de9a111506325
冯振 <18592067412@163.com>
2018-08-14 21:07:03 +0800

    这是第一次提交

--- src/org/westos/test/testDemo.java
+++ src/org/westos/test/testDemo.java
@@ -2,7 +2,8 @@ package org.westos.test;
 
 public class testDemo {
     public static void main(String[] args) {
-        System.out.println("这是一个测试！！！");
+        System.out.println("这是一个测试！！！！");
         System.out.println("这是第一次更新！！！");
+        System.out.println("这是第二次更新！！！");
     }
 }
