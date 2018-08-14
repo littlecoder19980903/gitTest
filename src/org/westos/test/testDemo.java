9332c72f41aa638ba1797c0fd876df52b36e0f0f
littlecoder19980903 <41882533+littlecoder19980903@users.noreply.github.com>
2018-08-14 20:49:07 +0800

    Update testDemo.java

--- src/org/westos/test/testDemo.java
+++ src/org/westos/test/testDemo.java
@@ -3,5 +3,6 @@ package org.westos.test;
 public class testDemo {
     public static void main(String[] args) {
         System.out.println("这是一个测试！！！");
+        System.out.println("这是第一次更新！！！");
     }
 }
